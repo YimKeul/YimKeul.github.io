4fa41c34b5a4a3d34dabf392be5a98a15dfa3dda
YimKeul <leesjun29@gmail.com>
2023-07-06 10:00:14 +0900

    feat : Experience timeline bar

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -12,6 +12,22 @@ const Experience = () => {
           Experience
           <S.bar />
         </S.section>
+        <S.tlBox>
+          <S.tlItem>
+            <div style={{ marginRight: "10rem" }}>
+              <span>2020</span>
+              <S.tlBarFillCircle />
+              <S.tlBarEmptyCircle />
+            </div>
+
+            <div>
+              <span>title</span>
+              <p>12312</p>
+            </div>
+          </S.tlItem>
+
+          <S.tlBar />
+        </S.tlBox>
       </S.inner>
     </S.container>
   );
@@ -35,6 +51,7 @@ const S = {
   inner: styled.div`
     margin-block: 3.25rem;
     padding-inline: 3.88rem;
+
     @media screen and (max-width: 767px) {
       margin: 0;
       padding-block: 3rem;
@@ -53,6 +70,48 @@ const S = {
     height: 0.25rem;
     background-color: ${() => point};
     border-radius: 1.25rem;
+  `,
+  tlBar: styled.span`
+    position: absolute;
+    width: 1px;
+    border-right: 2px solid #3aebb6;
+    top: 0;
+    bottom: 0;
+    left: 9.4rem;
+  `,
+  tlBarEmptyCircle: styled.span`
+    width: 14px;
+    height: 14px;
+    background-color: #fff;
+    border: 1px solid #3aebb6;
+    border-radius: 50%;
+    position: absolute;
+    /* top: 1rem; */
+    left: 9rem;
+    right: 0;
+    z-index: 1;
+  `,
+
+  tlBarFillCircle: styled.span`
+    width: 8px;
+    height: 8px;
+    background-color: #3aebb6;
+    border: 1px solid #3aebb6;
+    border-radius: 50%;
+    position: absolute;
+    top: 0.2rem;
+    left: 9.18rem;
+    z-index: 2;
+  `,
+
+  tlBox: styled.div`
+    position: relative;
+    margin-top: 2rem;
+  `,
+  tlItem: styled.div`
+    display: flex;
+    margin-bottom: 5rem;
+    position: relative;
   `,
 };
 
